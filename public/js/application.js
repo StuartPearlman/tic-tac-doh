546ce175a1ef65bdce8773fafaabeb8ff81d4ac5
Stuart Pearlman <stupearlman@gmail.com>
2014-09-23 15:21:29 -0500

    save difficulty

--- public/js/application.js
+++ public/js/application.js
@@ -8,6 +8,8 @@ $(document).ready(function() {
 
     , hard = false
 
+    , difficulty = localStorage.getItem('difficulty')
+
     , spotsTaken = []
 
     , playing = false
@@ -86,7 +88,13 @@ $(document).ready(function() {
 
     //Difficulty selection
 
+    previousDifficulty();
+
     $("#easy").click(function() {
+        easySetting();
+    });
+
+    function easySetting() {
         if (newGame == false) {
             easy = true;
             $(".easy").css('background', 'rgb(28, 184, 65)');
@@ -95,10 +103,15 @@ $(document).ready(function() {
             hard = false;
             $(".hard").css('background', 'gray');
             $(".subheading").replaceWith("<h2 class='subheading'>The game you can win.</h2>");
+            localStorage.setItem('difficulty', 'easy');
         };
-    });
+    };
 
     $("#medium").click(function() {
+        mediumSetting();
+    });
+
+    function mediumSetting() {
         if (newGame == false) {
             easy = false;
             $(".easy").css('background', 'gray');
@@ -107,10 +120,15 @@ $(document).ready(function() {
             hard = false;
             $(".hard").css('background', 'gray');
             $(".subheading").replaceWith("<h2 class='subheading'>The game you might win.</h2>");
+            localStorage.setItem('difficulty', 'medium');
         };
-    });
+    };
 
     $("#hard").click(function() {
+        hardSetting();
+    });
+
+    function hardSetting() {
         if (newGame == false) {
             easy = false;
             $(".easy").css('background', 'gray');
@@ -119,8 +137,21 @@ $(document).ready(function() {
             hard = true;
             $(".hard").css('background', 'rgb(202, 60, 60)');
             $(".subheading").replaceWith("<h2 class='subheading'>The game you won't win.</h2>");
+            localStorage.setItem('difficulty', 'hard');
         };
-    });
+    };
+
+    function previousDifficulty() {
+        if (difficulty == 'easy') {
+            easySetting();
+        };
+        if (difficulty == 'medium') {
+            mediumSetting();
+        };
+        if (difficulty == 'hard') {
+            hardSetting();
+        };
+    };
 
     //Game logic
 
